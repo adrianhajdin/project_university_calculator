a1c428a5697696f53dfbbd67316bbd0fc60cb3b1
adrian <adrianhajdin00@gmail.com>
2019-01-08 13:53:27 +0100

    remove uneccesarry

--- src/util/util.js
+++ src/util/util.js
@@ -46,13 +46,3 @@ export const calculateMaturaPoints = (percentage, evaluation, maturaLevel) => Ma
 export const calculatePercentages = (...args) => [...args].reduce((a, b) => parseFloat(a, 10) + parseFloat(b, 10), 0);
 export const calculateTotalGradePoints = (percentagesTotal, evaluationSchoolGrades) => Math.round((percentagesTotal / 4).toFixed(2) / 5 * evaluationSchoolGrades * 10);
 export const calculateTotalMaturaPoints = (...args) => [...args].reduce((a, b) => a + b, 0);
-
-export const func3 = () => {
-  // do stuff
-};
-export const func4 = () => {
-  // do stuff
-};
-export const func5 = () => {
-  // do stuff
-};
