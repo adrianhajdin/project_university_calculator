609f039ece8c7bb6eccc5de2f89640fb26bcda02
adrian <adrianhajdin00@gmail.com>
2019-01-16 17:25:23 +0100

    change

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const data = require('./documents/pdfs');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
