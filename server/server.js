edf6f9501786e3f2ea108fbca7680a50d4dab21e
adrian <adrianhajdin00@gmail.com>
2019-02-03 23:09:06 +0100

    heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pdf = require('html-pdf');
 const cors = require('cors');
+const path = require('path');
 
 const data = require('./documents');
 
@@ -11,6 +12,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
